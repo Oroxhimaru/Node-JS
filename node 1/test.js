9b6ef1544265cb16f9d8223bb4bd09a553372e2e
Oroxhimaru <hm51562@gmail.com>
2023-09-21 12:42:02 +0500

    nodeJS intro

---
+++ node 1/test.js
@@ -0,0 +1,15 @@
+const name = "M HASSAN MALIK";  // "UCHIHA ITACHI"; try this name
+
+console.log(name);
+
+/* no  need for browser console you can just check it in terminal by nodeJS by wrting node then file name e.g node test,  and it is going to show on the terminal */
+// try changing the name value
+
+
+// here how you can run function in node
+const greet = (dbzName) => {
+       console.log(`Hello, ${dbzName}`);
+}
+
+greet('Majin Vegeta');
+greet('Broly');
